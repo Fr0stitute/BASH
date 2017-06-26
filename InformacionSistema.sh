97b3ca70dc1c8ead17e68be3ee32d4adb5b781bc
Eddy Moreno <eddymoreno10@gmail.com>
2017-06-26 22:02:28 +0000

    Script agregado

---
+++ InformacionSistema.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+os=`uname -o`
+release=`uname -r`
+arq=`uname -i`
+
+echo El sistema es $os 
+echo La arquitectura es $arq
+echo El lanzamiento de kernel es $release
--- ListarArchivos.sh
+++ ListarArchivos.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# only if you have bash 4 in your CentOS system
 ls -Ash $1
