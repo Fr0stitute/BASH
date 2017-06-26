af3bde0e4b9f408d897fde286997bd600a10809e
Eddy Moreno <eddymoreno10@gmail.com>
2017-06-26 19:28:23 +0000

    Corregido cores number en InformacionCPU.sh

--- InformacionCPU.sh
+++ InformacionCPU.sh
@@ -3,7 +3,6 @@ cores=`grep "model name" /proc/cpuinfo | wc -l`
 modelo=`grep -m1 "model name" /proc/cpuinfo`
 cache=`grep -m1 "cache size" /proc/cpuinfo`
 GHz=`echo ${modelo#*@}`
-cores=$((cores-1))
 
 echo ${modelo%@*};
 echo cores : $cores 
